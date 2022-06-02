a99589a85f0c6e11fb912b3e25cb4baf27c91979
dp7639 <dp736116@gmail.com>
2022-06-02 13:57:17 +0530

    day one build

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon</title>
+      </Head>
+      <div>
+        <h1>Amazon</h1>
+      </div>
+
+      
+    </div>
+  );
+}
